415e1c32e3a39450e77aafcbac5ac4ea39481d3d
Ilpo Oksanen <ilpo.oksanen@outlook.com>
2016-12-03 13:46:41 +0200

    Added editorconfig and fixed&cleaned up methods&courses

--- js/courses.js
+++ js/courses.js
@@ -1,8 +1,8 @@
-import {rand, add} from './methods';
+import {rand, add, clone} from './methods';
 
 function generateCourses(){
   const courses=[],
-  			courseNames=[
+        courseNames=[
     ["Matematiikan", "Fysiikan", "Tietotekniikan", "Englannin", "Ruotsin", "Japanin", "Espanjan", "Ranskan", "Tietokantojen", "Mediatekniikan", "Käyttöliittymien", "Painotekniikan", "3D", "Olio-ohjelmoinnin", "Javan", "C#", "Javascriptin", "PHP", "Mobiiliohjelmoinnin", "Peliohjelmoinnin"],
     ["alkeet","jatkokurssi","perusteet","edistynyt kurssi"]
   ];
@@ -11,11 +11,11 @@ function generateCourses(){
   let id=0;
   for(let i=0; i<80; i++){
     const newCourse = {
-    	id:id++,
-   		name: 
-    		courseNames[0][rand(0,courseNames[0].length-1)] + " " +
-      	courseNames[1][rand(0,courseNames[1].length-1)],
-    	credits: rand(5),
+      id:id++,
+      name: 
+        courseNames[0][rand(0,courseNames[0].length-1)] + " " +
+        courseNames[1][rand(0,courseNames[1].length-1)],
+      credits: rand(5),
       edges: [],
     };
     const numberOfLessons = rand(3);
@@ -41,25 +41,25 @@ function generateCourses(){
 }
 
 function findEdges(courses){
-  courses=courses.clone();
+  courses=clone(courses);
   //Let's compare each course with one-other:
   for(let i=0; i<courses.length; i++){
-  	for(let j=i+1; j<courses.length; j++){
+    for(let j=i+1; j<courses.length; j++){
       checkEdge(i,j); //Löytyy riviltä 85
     }
   }
   
   //This function takes in two values, selects the courses corresponding to that values and check if any of the lessons associated to the courses overlap. If not, the index of each object in the courses array is stored in the other course's edges property, to indicate that they don't conflict with eachother.
   function checkEdge(a,b){
-  	//if "b" is not already in the edges of "a" and if courses a and b don't overlap:
-  	if(!~courses[a].edges.indexOf(b) && !coursesOverlap(courses[a],courses[b])){
+    //if "b" is not already in the edges of "a" and if courses a and b don't overlap:
+    if(!~courses[a].edges.indexOf(b) && !coursesOverlap(courses[a],courses[b])){
       courses[a].edges.insert(b);
       courses[b].edges.insert(a);
     }
   }
   
   return courses;
-	
+  
 }
 
 const startTime=Date.now();
@@ -72,10 +72,10 @@ courses=findEdges(courses);
 //returns true if there is overlapping, otherwise false
 function coursesOverlap(c1, c2){
   if(c1.id == c2.id) return true;
-	if(c1.period != c2.period) return false;
-	let overlaps=false;
-	loop1: for(i in c1.lessons) {
-    for(j in c2.lessons){
+  if(c1.period != c2.period) return false;
+  let overlaps=false;
+  loop1: for(let i in c1.lessons) {
+    for(let j in c2.lessons){
       if(lessonsOverlap(c1.lessons[i], c2.lessons[j])){
         overlaps=true;
         break loop1;
@@ -88,14 +88,14 @@ function coursesOverlap(c1, c2){
 //compares two lessons to see if their schedules overlap
 //returns true if there is overlapping, otherwise false
 function lessonsOverlap(l1, l2){
-	if(l1.day !== l2.day) return false;
+  if(l1.day !== l2.day) return false;
   const t = [l1,l2].sort((a, b) => a.startTime-b.startTime);
   if(t[1].startTime >= t[0].endTime) return false;
   return true;
 }
 
 function pickMoreValuable(c1,c2){
-	return c1.worth>c2.worth?c1:c2;
+  return c1.worth>c2.worth?c1:c2;
 }
 
 export default courses;
--- js/methods.js
+++ js/methods.js
@@ -1,47 +1,52 @@
 (()=>{
-	Array.prototype.spliced = function(){
-		Array.prototype.splice.apply(this, arguments);
-		return(this);
-	}
+  Array.prototype.spliced = function(){
+    Array.prototype.splice.apply(this, arguments);
+    return(this);
+  }
 
-	Array.prototype.spliced = function(){
-		Array.prototype.splice.apply(this, arguments);
-		return(this);
-	}
+  Array.prototype.spliced = function(){
+    Array.prototype.splice.apply(this, arguments);
+    return(this);
+  }
 
-	Array.prototype.insert=(position=>{
-	  return function(e){
-	  	this.splice(position(this, e), 0, e);
-	  }
-	})((array, value)=>{
-		let low = 0,
-			high = array.length;
-		while (low < high) {
-			const mid = low + high >>> 1;
-			if (array[mid] < value) low = mid + 1;
-			else high = mid;
-		}
-		return low;
-	});
+  Array.prototype.insert=(position=>{
+    return function(e){
+      this.splice(position(this, e), 0, e);
+    }
+  })((array, value)=>{
+    let low = 0,
+      high = array.length;
+    while (low < high) {
+      const mid = low + high >>> 1;
+      if (array[mid] < value) low = mid + 1;
+      else high = mid;
+    }
+    return low;
+  });
 
-	Object.prototype.clone = function(){
-		return JSON.parse(JSON.stringify(this));
-	}
+  // Object.prototype.cloned = function(){
+  //   return JSON.parse(JSON.stringify(this));
+  // }
 })();
 
+function clone(obj){
+  return JSON.parse(JSON.stringify(obj));
+}
+
 function rand(a, b=a){
-	if(b==a) a=1;
+  if(b==a) a=1;
   return ~~(Math.random()*(b-a+1)+a);
 }
 
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
-exports={
-	rand:rand,
-	add:add,
+const exports={
+  rand:rand,
+  add:add,
+  clone:clone,
 }
 
-export {rand, add};
+export {rand, add, clone};
 export default exports;
