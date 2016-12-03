66387d2ed50600193a8587af460f6c219487498d
Juho Jokela <juho.jokela@atalent.fi>
2016-12-03 13:17:46 +0200

    Import courses

--- components/pages/WorkLoadOptimator.js
+++ components/pages/WorkLoadOptimator.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Tools from '../Tools/Tools.js';
 import ResultList from '../ResultList/ResultList.js';
+import courses from '../../js/courses';
 
 import Banner from '../Banner.js';
 
 export class WorkLoadOptimator extends Component {
 
 	render() {
-
+		console.log(courses);
 		return (
 			<div className="container">
 				<Banner />
