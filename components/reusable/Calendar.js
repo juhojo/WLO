bc58f11e75b970aea4265a7f28f2ec36fdb73de7
Juho Jokela <juho.jokela@atalent.fi>
2016-12-13 11:10:09 +0200

    Change period onClick

--- components/reusable/Calendar.js
+++ components/reusable/Calendar.js
@@ -2,40 +2,23 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 import fullCalendar from 'fullcalendar';
 import moment from 'moment';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class Calendar extends Component {
-  render() {
-    return <div>
-      <button onClick="">1</button>
-      <div ref="calendar"></div>
-    </div>;
+  constructor() {
+    super();
+    this.state = { period: 1 };
   }
   componentWillReceiveProps(newProps) {
     const { courses } = this.props;
     if (newProps.courses !== courses && newProps.courses.length > 0) {
-      newProps.courses.forEach((course, i) => {
-        if(course.period==2){
-          course.lessons.forEach((lesson, j) => {
-            // console.log(lesson);
-            console.log(lesson);
-            const startTime = moment().startOf('isoweek').add(lesson.day-1, 'days').add(lesson.startTime, 'hours').toDate();
-            const endTime = moment().startOf('isoweek').add(lesson.day-1, 'days').add(lesson.endTime, 'hours').toDate();
-            console.log(startTime); 
-            $(this.refs.calendar).fullCalendar('renderEvent',
-              {
-                id: course.id,
-                title: course.name,
-                start: startTime, // lesson.startTime
-                end: endTime,
-              },
-            );
-          });
-        }
-      });
+      this.updateView(newProps.courses);
     }
   }
   componentDidMount() {
     $(this.refs.calendar).fullCalendar({
+      header: false,
+      columnFormat: 'ddd',
 			editable: true,
       defaultView: 'agendaWeek',
       hiddenDays: [ 0, 6 ], // hide Sundays and Saturdays
@@ -43,15 +26,52 @@ export default class Calendar extends Component {
       allDaySlot: false,
       minTime: "08:00:00",
       maxTime: "18:00:00",
-			droppable: true, // this allows things to be dropped onto the calendar
-			drop: function() {
-				// is the "remove after drop" checkbox checked?
-				if ($('#drop-remove').is(':checked')) {
-					// if so, remove the element from the "Draggable Events" list
-					$(this).remove();
-				}
-			}
     });
+  }
+  componentDidUpdate(oldProps, oldState) {
+    if (oldState.period !== this.state.period) {
+      this.updateView(this.props.courses);
+    }
+  }
+  changePeriod(i) {
+    this.setState({ period: i });
+  }
+  renderButtons() {
+    return Array(4).fill(0).map((v, i) =>
+      <RaisedButton key={i}
+        style={{margin: '5px'}}
+        label={i+1}
+        primary={true}
+        onTouchTap={this.changePeriod.bind(this, i+1)}/>
+    );
+  }
+  updateView(courses) {
+    $(this.refs.calendar).fullCalendar('removeEvents');
+    courses.forEach((course, i) => {
+      if(course.period==this.state.period){
+        course.lessons.forEach((lesson, j) => {
+          // console.log(lesson);
+          console.log(lesson);
+          const startTime = moment().startOf('isoweek').add(lesson.day-1, 'days').add(lesson.startTime, 'hours').toDate();
+          const endTime = moment().startOf('isoweek').add(lesson.day-1, 'days').add(lesson.endTime, 'hours').toDate();
+          console.log(startTime);
+          $(this.refs.calendar).fullCalendar('renderEvent',
+            {
+              id: course.id,
+              title: course.name,
+              start: startTime, // lesson.startTime
+              end: endTime,
+            },
+          );
+        });
+      }
+    });
+  }
+  render() {
+    return <div>
+      {this.renderButtons()}
+      <div ref="calendar"></div>
+    </div>;
   }
 }
 
